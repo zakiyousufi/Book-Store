69459f7517857310e1422d1517f2d8b9c4915320
Ahmad Zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-09-20 20:38:20 +0430

    Revert "Add reducers and actions"

--- src/Components/CategoriesContainer.js
+++ src/Components/CategoriesContainer.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux/es/exports';
 import Navbar from './Navbar';
-import { checkStatus } from '../redux/categories/categories';
 
-const CategoriesContainer = () => {
-  const state = useSelector((state) => state.categories);
-  const dispatch = useDispatch();
-  return (
-    <div className="categoriesContainer">
-      <Navbar />
-      <button type="button" onClick={() => dispatch(checkStatus())}>Check Status</button>
-      <h2>{state || null }</h2>
-    </div>
-  );
-};
+const CategoriesContainer = () => (
+  <div className="categoriesContainer">
+    <Navbar />
+    <button type="button">Check Status</button>
+  </div>
+);
 
 export default CategoriesContainer;
