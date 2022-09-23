3772328d86eb773216e947ba8d2494ca36001cf1
Ahmad Zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-09-23 18:00:04 +0430

    Revert "API connection"

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore';
+import { createStore } from '@reduxjs/toolkit';
+import rootReducer from './redux/configureStore';
+import './index.css';
 import App from './App';
-import './App.css';
+import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+      <App />
+    </Provider>,
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
